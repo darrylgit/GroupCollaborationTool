0caf889de56691d4f714f3f5a5044c97f73f7bc2
darrylgit <darrylsmithup@gmail.com>
2020-01-11 09:00:50 -0500

    Improve the SignUp/SignIn/SignOut UI.

--- group-collaboration/src/components/Landing/index.js
+++ group-collaboration/src/components/Landing/index.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {SessionContext} from '../Session'
+import * as ROUTES from '../../constants/routes';
 
 export default function() {
+  const session = useContext(SessionContext)
+
   return (
   <div>
     <h1>WebDevAtlanta: Groopa</h1>
     <p>
-      Hello! This is project for WebDevAtlanta. <a href="https://github.com/webdevatlanta/GroupCollaborationTool">See the code here</a>
+      Hello! This is a tool for collaboration. It is a WebDevAtlanta project. <a href="https://github.com/webdevatlanta/GroupCollaborationTool">See the code here</a>.
     </p>
+    { !session &&
+      <p> You are not currently signed in. If you have an account, <a href={ROUTES.SIGN_IN}>log in here</a>. Otherwise, <a href={ROUTES.SIGN_UP}>sign up</a>!</p>
+    }
+    { session &&
+      <p>You are signed in.</p>
+    }
   </div>
   )
 }
--- group-collaboration/src/components/TopBar/index.js
+++ group-collaboration/src/components/TopBar/index.js
@@ -5,14 +5,10 @@ import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
-import Badge from '@material-ui/core/Badge';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
 import Button from '@material-ui/core/Button';
 
@@ -95,10 +91,6 @@ export default function PrimarySearchAppBar() {
   const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
-  const handleProfileMenuOpen = event => {
-    setAnchorEl(event.currentTarget);
-  };
-
   const handleMobileMenuClose = () => {
     setMobileMoreAnchorEl(null);
   };
@@ -137,7 +129,7 @@ export default function PrimarySearchAppBar() {
       <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
       <MenuItem onClick={handleMenuClose}>My account</MenuItem>
     </Menu>
-  );
+  )
 
   const mobileMenuId = 'primary-search-account-menu-mobile';
   const renderMobileMenu = (
@@ -152,87 +144,72 @@ export default function PrimarySearchAppBar() {
     >
       { session &&
         <>
-          <MenuItem>
-            <IconButton aria-label="show 4 new mails" color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <p>Messages</p>
-          </MenuItem>
-          <MenuItem>
-            <IconButton aria-label="show 11 new notifications" color="inherit">
-              <Badge badgeContent={11} color="secondary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-            <p>Notifications</p>
-          </MenuItem>
-          <MenuItem onClick={handleProfileMenuOpen}>
-            <IconButton
-              aria-label="account of current user"
-              aria-controls="primary-search-account-menu"
-              aria-haspopup="true"
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-            <p>Profile</p>
-          </MenuItem>
           <MenuItem onClick={handleSignOut}>Sign Out</MenuItem>
         </>
       }
       { !session &&
-        <MenuItem onClick={handleSignIn}>Sign In</MenuItem>
+        <MenuItem onClick={handleSignIn}>Login</MenuItem>
       }
     </Menu>
-  );
+  )
+
+  const renderAppBar = (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="open drawer"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography className={classes.title} variant="h6" noWrap>
+          Material-UI
+        </Typography>
+        <div className={classes.search}>
+          <div className={classes.searchIcon}>
+            <SearchIcon />
+          </div>
+          <InputBase
+            placeholder="Search…"
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+            inputProps={{ 'aria-label': 'search' }}
+          />
+        </div>
+        <div className={classes.grow} />
+        <div className={classes.sectionDesktop}>
+          { !session &&
+            <Button color="inherit" onClick={handleSignIn}>Login</Button>
+          }
+          { session &&
+            <p><small>{session.email}</small></p>
+          }
+        </div>
+        <div className={classes.sectionMobile}>
+          { session &&
+            <p><small>{session.email}</small></p>
+          }
+          <IconButton
+            aria-label="show more"
+            aria-controls={mobileMenuId}
+            aria-haspopup="true"
+            onClick={handleMobileMenuOpen}
+            color="inherit"
+          >
+            <MoreIcon />
+          </IconButton>
+        </div>
+      </Toolbar>
+    </AppBar>
+  )
 
   return (
     <div className={classes.grow}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography className={classes.title} variant="h6" noWrap>
-            Material-UI
-          </Typography>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </div>
-          <div className={classes.grow} />
-          <div className={classes.sectionDesktop}>
-            <Button color="inherit">Login</Button>
-          </div>
-          <div className={classes.sectionMobile}>
-            <IconButton
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
-          </div>
-        </Toolbar>
-      </AppBar>
+      {renderAppBar}
       {renderMobileMenu}
       {renderMenu}
     </div>
