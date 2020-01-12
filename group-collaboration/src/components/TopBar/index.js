11e868bcc05916bb67ad638d7eae6f8bf530c204
darrylgit <darrylsmithup@gmail.com>
2020-01-11 21:02:11 -0500

    Better (non-boilerplate) title and meta description.

--- group-collaboration/src/components/TopBar/index.js
+++ group-collaboration/src/components/TopBar/index.js
@@ -163,7 +163,7 @@ export default function PrimarySearchAppBar() {
           <MenuIcon />
         </IconButton>
         <Typography className={classes.title} variant="h6" noWrap>
-          Material-UI
+          Groopa
         </Typography>
         <div className={classes.search}>
           <div className={classes.searchIcon}>
