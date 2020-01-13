2782b0feadb816912cfd107dd397acbd437c8b75
darrylgit <darrylsmithup@gmail.com>
2020-01-13 07:58:54 -0500

    Implement email verification button.

---
+++ group-collaboration/src/components/Profile/emailVerifier.js
@@ -0,0 +1,37 @@
+import React, {useContext, useState} from 'react'
+import {SessionContext} from '../Session'
+import Button from '@material-ui/core/Button'
+
+export default function() {
+  const session = useContext(SessionContext)
+  const [verifying, setVerifying] = useState(false)
+
+  const handleVerify = (event) => {
+    event.preventDefault();
+    session.sendEmailVerification({
+      handleCodeInApp: true,
+      url: process.env.REACT_APP_CONFIRMATION_EMAIL_REDIRECT,
+    });
+    setVerifying(true);
+  }
+
+  if(session.emailVerified) {
+    return (
+      <div>
+        {session.email} &#x2714;
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        {session.email}
+        {verifying &&
+          <Button color="secondary" >Verifying...</Button>
+        }
+        {!verifying &&
+          <Button color="primary" onClick={handleVerify}>click to verify</Button>
+        }
+      </div>
+    )
+  }
+}
--- group-collaboration/src/components/Profile/index.js
+++ group-collaboration/src/components/Profile/index.js
@@ -1,22 +1,26 @@
 import React, {useContext} from 'react'
 import {SessionContext} from '../Session'
-import DisplayNameForm from './displayNameForm'
+import EmailVerifier from './emailVerifier'
+import NameEditor from './nameEditor'
 
 export default function() {
   const session = useContext(SessionContext)
 
-  const hasVerifiedEmail = () => session.emailVerified
-
-  return (
-    <div>
-      <h1>Profile</h1>
-      { session &&
-        <>
-          <p>Email: {session.email}</p>
-          <p>Email Verified: {hasVerifiedEmail() ? "yes" : "no"}</p>
-          <DisplayNameForm/>
-        </>
-      }
-    </div>
-  )
+  if (session) {
+    return (
+      <div>
+        <h1>Profile</h1>
+        { session &&
+          <>
+            <EmailVerifier/>
+            <NameEditor/>
+          </>
+        }
+      </div>
+    )
+  } else {
+    return (
+      <a href="/signin">Please sign in.</a>
+    )
+  }
 }
