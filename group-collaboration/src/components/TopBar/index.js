9adeb25ed938f3429cda9de224641b5cd8c27d1f
darrylgit <darrylsmithup@gmail.com>
2020-01-21 08:30:44 -0500

    Auto-formatted sourcefile.

--- group-collaboration/src/components/TopBar/index.js
+++ group-collaboration/src/components/TopBar/index.js
@@ -1,89 +1,89 @@
-import React, {useContext} from 'react';
-import { fade, makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import MoreIcon from '@material-ui/icons/MoreVert';
-import Button from '@material-ui/core/Button';
+import React, { useContext } from "react";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import MoreIcon from "@material-ui/icons/MoreVert";
+import Button from "@material-ui/core/Button";
 
 import { useHistory } from "react-router-dom";
-import * as ROUTES from '../../constants/routes';
-import {FirebaseContext} from '../Firebase'
-import {SessionContext} from '../Session'
-import NameTag from './nametag'
+import * as ROUTES from "../../constants/routes";
+import { FirebaseContext } from "../Firebase";
+import { SessionContext } from "../Session";
+import NameTag from "./nametag";
 
 const useStyles = makeStyles(theme => ({
   grow: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(2)
   },
   title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block"
+    }
   },
   search: {
-    position: 'relative',
+    position: "relative",
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
     },
     marginRight: theme.spacing(2),
     marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing(3),
-      width: 'auto',
-    },
+      width: "auto"
+    }
   },
   searchIcon: {
     width: theme.spacing(7),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
   },
   inputRoot: {
-    color: 'inherit',
+    color: "inherit"
   },
   inputInput: {
     padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: 200,
-    },
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: 200
+    }
   },
   sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-    },
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex"
+    }
   },
   sectionMobile: {
-    display: 'flex',
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
-    },
-  },
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none"
+    }
+  }
 }));
 
 export default function PrimarySearchAppBar() {
-  const firebase = useContext(FirebaseContext)
-  const session = useContext(SessionContext)
-  const history = useHistory()
+  const firebase = useContext(FirebaseContext);
+  const session = useContext(SessionContext);
+  const history = useHistory();
 
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -107,50 +107,46 @@ export default function PrimarySearchAppBar() {
 
   const handleSignIn = event => {
     handleMenuClose();
-    history.push(ROUTES.SIGN_IN)
-  }
+    history.push(ROUTES.SIGN_IN);
+  };
 
   const handleSignOut = event => {
     handleMenuClose();
     firebase.doSignOut();
-    history.push(ROUTES.LANDING)
-  }
+    history.push(ROUTES.LANDING);
+  };
 
-  const menuId = 'primary-search-account-menu';
+  const menuId = "primary-search-account-menu";
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
       id={menuId}
       keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
       <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
       <MenuItem onClick={handleMenuClose}>My account</MenuItem>
     </Menu>
-  )
+  );
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const mobileMenuId = "primary-search-account-menu-mobile";
   const renderMobileMenu = (
     <Menu
       anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
       id={mobileMenuId}
       keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
-      { session.user &&
-        <MenuItem onClick={handleSignOut}>Sign Out</MenuItem>
-      }
-      { !session.user &&
-        <MenuItem onClick={handleSignIn}>Login</MenuItem>
-      }
+      {session.user && <MenuItem onClick={handleSignOut}>Sign Out</MenuItem>}
+      {!session.user && <MenuItem onClick={handleSignIn}>Login</MenuItem>}
     </Menu>
-  )
+  );
 
   const renderAppBar = (
     <AppBar position="static">
@@ -174,20 +170,22 @@ export default function PrimarySearchAppBar() {
             placeholder="Search…"
             classes={{
               root: classes.inputRoot,
-              input: classes.inputInput,
+              input: classes.inputInput
             }}
-            inputProps={{ 'aria-label': 'search' }}
+            inputProps={{ "aria-label": "search" }}
           />
         </div>
         <div className={classes.grow} />
         <div className={classes.sectionDesktop}>
-          { !session.user &&
-            <Button color="inherit" onClick={handleSignIn}>Login</Button>
-          }
-          { session.user && <NameTag/> }
+          {!session.user && (
+            <Button color="inherit" onClick={handleSignIn}>
+              Login
+            </Button>
+          )}
+          {session.user && <NameTag />}
         </div>
         <div className={classes.sectionMobile}>
-          { session.user && <NameTag/> }
+          {session.user && <NameTag />}
           <IconButton
             aria-label="show more"
             aria-controls={mobileMenuId}
@@ -200,7 +198,7 @@ export default function PrimarySearchAppBar() {
         </div>
       </Toolbar>
     </AppBar>
-  )
+  );
 
   return (
     <div className={classes.grow}>
