9b04d3975b48615772a5aac3769817d3889b712c
darrylgit <darrylsmithup@gmail.com>
2020-01-22 20:21:45 -0500

    Style signin

--- group-collaboration/src/components/SignIn/form.js
+++ group-collaboration/src/components/SignIn/form.js
@@ -1,34 +1,31 @@
-import React, {useState, useContext} from 'react'
-import {useHistory} from 'react-router-dom'
-import * as ROUTES from '../../constants/routes';
-import {FirebaseContext} from '../Firebase'
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import * as ROUTES from "../../constants/routes";
+import { FirebaseContext } from "../Firebase";
 
 export default function() {
-  const history = useHistory()
-  const firebase = useContext(FirebaseContext)
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [error, setError] = useState("")
+  const history = useHistory();
+  const firebase = useContext(FirebaseContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  const onSubmit = (event) => {
-    event.preventDefault()
+  const onSubmit = event => {
+    event.preventDefault();
 
     firebase
       .doSignInWithEmailAndPassword(email, password)
-      .then( user => history.push(ROUTES.LANDING) )
-      .catch( error => setError(error) )
-  }
+      .then(user => history.push(ROUTES.LANDING))
+      .catch(error => setError(error));
+  };
 
   const validate = () => {
-    return (
-      email.length > 0 &&
-      password.length > 0
-    )
-  }
+    return email.length > 0 && password.length > 0;
+  };
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
+    <>
+      <form className="signin-form" onSubmit={onSubmit}>
         <input
           autoFocus
           type="email"
@@ -42,10 +39,20 @@ export default function() {
           value={password}
           onChange={e => setPassword(e.target.value)}
         />
-        <input type="submit" value="Sign In" disabled={!validate()} />
+        <button
+          type="submit"
+          className={
+            validate()
+              ? "signin-form-submit validated"
+              : "signin-form-submit disabled"
+          }
+          disabled={!validate()}
+        >
+          Sign In
+        </button>
 
         {error && <p>{error.message}</p>}
       </form>
-    </div>
-  )
+    </>
+  );
 }
