2ca9ab4a67ab9dcad08a1c76a645ac3c36652572
darrylgit <darrylsmithup@gmail.com>
2020-02-13 20:11:16 -0500

    Remove deprecated code

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,32 +3,7 @@ import ProjectsTable from './table';
 import './styles.css';
 import { FirebaseContext } from '../Firebase';
 
-export default function(props) {
-  // if (props.storybook) {
-  //   const projects = [
-  //     {
-  //       description: 'This is a public project',
-  //       name: 'Example 1',
-  //       owner: '0',
-  //       type: 'Public',
-  //       id: '0'
-  //     },
-  //     {
-  //       description: 'This is a private project',
-  //       name: 'Example 2',
-  //       owner: '0',
-  //       type: 'Private',
-  //       id: '0'
-  //     }
-  //   ];
-  //   return (
-  //     <div style={{ marginTop: '30px' }}>
-  //       <div>Group Collaboration</div>
-  //       <ProjectsTable projects={projects} />
-  //     </div>
-  //   );
-  // }
-
+export default function() {
   const firebase = useContext(FirebaseContext);
   console.log(firebase);
   const [projects, setProjects] = useState([]);
