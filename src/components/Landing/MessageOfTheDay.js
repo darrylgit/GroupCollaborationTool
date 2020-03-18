062bd468c1191dd6ebfc030b15dd3b20e22d1c48
darrylgit <darrylsmithup@gmail.com>
2020-03-18 15:43:28 -0400

    Refactor Landing page to handle undefined, null, and valid sessions.

---
+++ src/components/Landing/MessageOfTheDay.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function() {
+  return (
+    <p>
+      Hello! This is a tool for collaboration. It is a WebDevAtlanta project.{" "}
+      <a href="https://github.com/webdevatlanta/GroupCollaborationTool">
+        See the code here
+      </a>
+    </p>
+  );
+}
